b42874c6050167dad37727c629519a092ecb4920
ahmadfareed <a.fareed@pioneerlogics.com>
2022-05-06 15:54:43 +0500

    model functionality added

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 
 const gradChnange = document.querySelector(".svg__grad");
 const rightPanel = document.querySelector(".right__panel");
+const addNewBtn = document.querySelector(".add__new__btn");
+const model = document.querySelector(".model");
+const overlay = document.querySelector(".overlay");
+const closeModel = document.querySelector(".close__model");
 
 let count = 0;
+
+//////////// EVENT HANDLERS //////////
 
 gradChnange.addEventListener("click", function () {
   count++;
@@ -15,4 +21,14 @@ gradChnange.addEventListener("click", function () {
     rightPanel.classList.remove(`grad__${count - 1}`);
     rightPanel.classList.add(`grad__${count}`);
   }
+});
+
+addNewBtn.addEventListener("click", function () {
+  model.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+closeModel.addEventListener("click", function () {
+  model.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
